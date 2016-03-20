3de8d28a5640fb28860d0e4687da9fcacf1be927
Marcin Puchlik <puchlikmarcin@gmail.com>
2016-03-20 15:04:50 +0100

    This is out first commit

---
+++ app/src/main/java/none/training_3/Rules.java
@@ -0,0 +1,15 @@
+package none.training_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Marcin on 20.03.2016.
+ */
+public class Rules extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
